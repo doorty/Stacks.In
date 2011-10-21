b1f17335993f2eba0c172f6e7c85acbfa03111fc
Brent Daugherty <brent@doorty.com>
2011-10-21 16:40:58 -0700

    tweaking design. added new layout for other home pages, e.g., about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,19 @@
 class HomeController < ApplicationController
-	layout "home"
+	layout :choose_layout
 	before_filter :require_login, :only => :secret
 	
   def index
   end
 
   def about
+  end
+
+  def choose_layout
+  	if ['index'].include? action_name
+  		'home_index'
+  	else
+  		'home'
+  	end
   end
 
 end
